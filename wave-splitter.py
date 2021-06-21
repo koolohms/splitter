6e7424693f9dcdcdc0b5c97840df3dcc65d82be0
Justin Turcotte <justin@Mystique.local>
2021-06-21 02:12:23 -0400

    added functionality to also print receipts to the thermal printer

--- wave-splitter.py
+++ wave-splitter.py
@@ -4,12 +4,27 @@
 # ESE
 # June 20, 2021
 
+# Printer information
+# Vendor ID:Product Code = 0416:5011
+# Interface number = 4
+# Output endpoint address = 3
+
 import os
 import time
+import sys
+escposModulePath = "/home/pi/.local/lib/python3.7/site-packages"
+sys.path.append(escposModulePath)
+from escpos.printer import Usb
+import pdftotext
 
 # Important paths
 POS_PRINT_DEST = "/var/spool/cups-pdf/ANONYMOUS/"
 RECEIPT_DIR = "/media/pi/TESTUSB/"
+
+# Thermal printer init
+p = Usb(0x0416, 0x5011, 0, 0x04, 0x03)
+
+
 
 while True:
     pdfs = os.listdir(POS_PRINT_DEST)
@@ -19,6 +34,11 @@ while True:
         fw = open(RECEIPT_DIR+pdfs[i], 'wb')
 
         fw.write(fr.read()) # write pdf to NFC device
+
+        pdf = pdftotext.PDF(fr)
+        text = "\n\n".join(pdf)
+        p.text(text)
+        p.cut()
 
         fr.close()
         fw.close()
