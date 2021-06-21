b15da16324699b37c2c54d15194dd376366b2a19
Justin Turcotte <justin@Mystique.local>
2021-06-21 01:40:34 -0400

    created python script that will write receipts to the NFC device

--- wave-splitter.py
+++ wave-splitter.py
@@ -3,3 +3,25 @@
 # Capstone
 # ESE
 # June 20, 2021
+
+import os
+import time
+
+# Important paths
+POS_PRINT_DEST = "/var/spool/cups-pdf/ANONYMOUS/"
+RECEIPT_DIR = "/media/pi/TESTUSB/"
+
+while True:
+    pdfs = os.listdir(POS_PRINT_DEST)
+
+    for i in range(0,len(pdfs)):
+        fr = open(POS_PRINT_DEST+pdfs[i], 'rb')
+        fw = open(RECEIPT_DIR+pdfs[i], 'wb')
+
+        fw.write(fr.read()) # write pdf to NFC device
+
+        fr.close()
+        fw.close()
+
+        os.remove(POS_PRINT_DEST+pdfs[i])
+    time.sleep(1)
