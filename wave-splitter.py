f10750cc3854a023696f4093a0c17266a6582f4d
Justin Turcotte <justin@Mystique.local>
2021-06-21 02:45:15 -0400

    revised script to allow switching between NFC or printer output

--- wave-splitter.py
+++ wave-splitter.py
@@ -16,6 +16,7 @@ escposModulePath = "/home/pi/.local/lib/python3.7/site-packages"
 sys.path.append(escposModulePath)
 from escpos.printer import Usb
 import pdftotext
+import RPi.GPIO as GPIO
 
 # Important paths
 POS_PRINT_DEST = "/var/spool/cups-pdf/ANONYMOUS/"
@@ -24,24 +25,32 @@ RECEIPT_DIR = "/media/pi/TESTUSB/"
 # Thermal printer init
 p = Usb(0x0416, 0x5011, 0, 0x04, 0x03)
 
-
+# GPIO
+# 0 -> NFC device
+# 1 -> Printer device
+GPIO_PIN = 25   # We're using GPIO "BCM 25"
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.IN)
 
 while True:
     pdfs = os.listdir(POS_PRINT_DEST)
 
     for i in range(0,len(pdfs)):
+        state = GPIO.input(GPIO_PIN)
+
         fr = open(POS_PRINT_DEST+pdfs[i], 'rb')
-        fw = open(RECEIPT_DIR+pdfs[i], 'wb')
 
-        # fw.write(fr.read()) # write pdf to NFC device
+        if state == 0:
+            fw = open(RECEIPT_DIR+pdfs[i], 'wb')
+            fw.write(fr.read()) # write pdf to NFC device
+            fw.close()
 
-        pdf = pdftotext.PDF(fr)
-        text = "\n\n".join(pdf)
-        p.text(text)
-        p.cut()
+        elif state == 1:
+            pdf = pdftotext.PDF(fr)
+            text = "\n\n".join(pdf)
+            p.text(text)
+            p.cut()
 
         fr.close()
-        fw.close()
-
         os.remove(POS_PRINT_DEST+pdfs[i])
     time.sleep(1)
