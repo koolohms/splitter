a170cf01474655a198741defb5610933758fdc50
Justin Turcotte <justin@kturcotte.me>
2021-07-05 00:53:58 -0400

    changed log output to be in the home directory to avoid permission issues

--- wave-launcher.sh
+++ wave-launcher.sh
@@ -2,17 +2,18 @@
 # wave-launcher.sh
 # launches waves splitter process
 
-LOGFILE=/etc/cups/postprocessing/wave.log
+LOGFILE=/home/pi/wave.log
+PYTHONFILE=/home/pi/Documents/splitter
 
 (
     echo "$(date "+%m%d%Y %T") : Wave online"
 ) > $LOGFILE
 
 cd /
-cd /home/pi/Documents/splitter
+cd $PYTHONFILE
 sudo python3 wave-splitter.py
 cd /
 
 (
-    echo "$(date "+%m%d%Y %T) : Wave process complete. Exiting.."
+    echo "$(date "+%m%d%Y %T") : Wave process complete. Exiting.."
 ) > $LOGFILE
